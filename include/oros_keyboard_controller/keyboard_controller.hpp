7b360e08c33992a4eea3fc759eafb2858bf338fd
oriros <bggh9395@gmail.com>
2024-03-04 18:58:57 +0900

    hi key

--- include/oros_keyboard_controller/keyboard_controller.hpp
+++ include/oros_keyboard_controller/keyboard_controller.hpp
@@ -1,11 +1,14 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include <unistd.h>
 #include <iostream>
+#include <termios.h>
 class KeyboardControllerNode : public rclcpp::Node
 {
     public:
         KeyboardControllerNode();
     private:
         rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
-
+        int get_key();
+        char key(' ');
 };
--- src/keyboard_controller_node.cpp
+++ src/keyboard_controller_node.cpp
@@ -20,3 +20,30 @@ KeyboardControllerNode::KeyboardControllerNode():Node("keyboard_controller"){
 )";
   publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
 }
+#include <stdio.h>
+#include <unistd.h>
+void KeyboardControllerNode::get_key(){
+  int ch;
+  struct termios oldt;
+  struct termios newt;
+
+  // Store old settings, and copy to new settings
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  // Make required changes and apply the settings
+  newt.c_lflag &= ~(ICANON | ECHO);
+  newt.c_iflag |= IGNBRK;
+  newt.c_iflag &= ~(INLCR | ICRNL | IXON | IXOFF);
+  newt.c_lflag &= ~(ICANON | ECHO | ECHOK | ECHOE | ECHONL | ISIG | IEXTEN);
+  newt.c_cc[VMIN] = 1;
+  newt.c_cc[VTIME] = 0;
+  tcsetattr(fileno(stdin), TCSANOW, &newt);
+
+  // Get the current character
+  ch = getchar();
+
+  // Reapply old settings
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+  return ch;
+}
